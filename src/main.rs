46993871e0c0e75559a097c77da6166a76b06d42
Gus <angusedmunds1@gmail.com>
2023-10-08 00:07:38 +0100

    added wildcard

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,19 @@ use std::{convert::Infallible, net::SocketAddr, time::Duration};
 
 use axum::{
     body::{Body, Bytes},
-    extract::{Path, State, ConnectInfo},
+    extract::{Path, State, ConnectInfo, Host, OriginalUri},
     response::{IntoResponse, Response},
-    routing::get,
+    routing::{get, any},
     Json, Router, Server,
 };
-use reqwest::{Client, StatusCode};
 use tokio_stream::StreamExt;
 use tower_http::trace::TraceLayer;
 use tracing::Span;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 // HOST / SERVER ADDRESS
-const HOST_ADDR: &str = "127.0.0.1";
-const HOST_PORT: &str = "3000";
+const HOST_ADDR: &str = "0.0.0.0";
+const HOST_PORT: &str = "1081";
 
 // Where we are redirecting the requests
 const SEND_ADDR: &str = "127.0.0.1";
@@ -32,7 +31,8 @@ async fn main() {
 
     // Create routes w/ states
     let app = Router::new()
-        .route("/:path/:path/:path", get(proxy));
+        .route("/*O", get(wildcard))
+        .route("/direct", get(direct));
     // Start Server
     axum::Server::bind(&address.parse().unwrap())
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
@@ -40,7 +40,29 @@ async fn main() {
         .unwrap();
 }
 
-async fn proxy(Path(path): Path<String>, ConnectInfo(addr): ConnectInfo<SocketAddr>,) -> impl IntoResponse {
-    println!("Received:\n{path}\nFrom:\n{addr}\n");
-    format!("{path}\n")
+async fn wildcard(
+    Path(path): Path<String>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    OriginalUri(original_uri): OriginalUri,
+    ) -> impl IntoResponse {
+    println!("Received:\n{original_uri}\nFrom:\n{addr}\n");
+    format!("echo: {path}\n")
 }
+
+async fn direct(
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    OriginalUri(original_uri): OriginalUri,
+    ) -> impl IntoResponse {
+    println!("Received:\n{original_uri}\nFrom:\n{addr}\n");
+    format!("Hello Proxee\n")
+}
+
+// async fn example(
+//     Path(path): Path<String>,
+//     ConnectInfo(addr): ConnectInfo<SocketAddr>,
+//     OriginalUri(original_uri): OriginalUri,
+//     ) -> impl IntoResponse {
+//     println!("{original_uri}"); -> "/api/x/y/z"
+//     println!("{path}"); -> "api/x/y/z"
+
+// }
