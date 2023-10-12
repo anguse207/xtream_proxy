c382a30d980ce8530b63847152214e220aff4c91
Angus Edmunds <angusedmunds1@gmail.com>
2023-10-12 23:38:49 +0100

    custom user and pass

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{net::SocketAddr, time::Duration};
 
 use axum::{
     body::StreamBody,
-    extract::{OriginalUri, ConnectInfo},
+    extract::{ConnectInfo, OriginalUri},
     response::IntoResponse,
     routing::get,
     Router,
@@ -21,16 +21,19 @@ const HOST_ADDR: &str = "0.0.0.0";
 const HOST_PORT: &str = "1081";
 
 // TARGET
-const XC_ADDR: &str = "http://thelads.ddns.net:80";
-const _XC_USER: &str = "jN9AhFAHmf";
-const _XC_PASS: &str = "r9amExT7Qm";
+const XT_ADDR: &str = "http://thelads.ddns.net:80";
+const XT_USER: &str = "jN9AhFAHmf";
+const XT_PASS: &str = "r9amExT7Qm";
+
+const USER: &str = "citrus";
+const PASS: &str = "fire";
 
 static CLIENT: Lazy<Client> = Lazy::new(|| {
     reqwest::Client::builder()
-    .user_agent(USER_AGENT)
-    .timeout(Duration::from_secs(360))
-    .build()
-    .unwrap()
+        .user_agent(USER_AGENT)
+        .timeout(Duration::from_secs(360))
+        .build()
+        .unwrap()
 });
 
 #[tokio::main]
@@ -45,7 +48,7 @@ async fn main() {
 
     // Create routes w/ states
     let app = Router::new().route("/*O", get(proxy));
-    
+
     let bind_address = format!("{}:{}", HOST_ADDR, HOST_PORT);
     tracing::info!("listening on {bind_address}");
 
@@ -57,12 +60,17 @@ async fn main() {
 }
 
 async fn proxy(
-    OriginalUri(original_uri): OriginalUri,
+    OriginalUri(path): OriginalUri,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
 ) -> impl IntoResponse {
-    tracing::info!("{addr} => {original_uri}");
+    // If using USER/PASS, replace it with XT_USER/XT_PASS
+    let path = format!("{path}")
+        .replace(USER, XT_USER)
+        .replace(PASS, XT_PASS);
 
-    let target: String = format!("{XC_ADDR}{original_uri}");
+    let target: String = format!("{XT_ADDR}{path}");
+
+    tracing::info!("{addr} => {path}");
 
     let response = CLIENT.get(target).send().await.unwrap();
 
